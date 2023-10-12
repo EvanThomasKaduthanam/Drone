a8b85bdf2ec4c81c80a4872d21dc5b87603466ac
nuclearzzet <nikettjohn@outlook.com>
2023-10-13 00:36:04 +0530

    🐛 Caaught and fixed bugs

--- Flight/Flight.ino
+++ Flight/Flight.ino
@@ -1,30 +1,42 @@
 #include <Servo.h>
+#include <Wire.h>
 
 #define MIN_PULSE_LENGTH 1000
 #define MAX_PULSE_LENGTH 2000
 
-Servo motA, motB, motC, motD;
 char data;
+
+struct Motors
+{
+  Servo motFL;
+  Servo motFR;
+  Servo motBL;
+  Servo motBR;
+
+} motors;
 
 void setup()
 {
+  Serial.begin(0);
   int iResult = checkWiring();
   if (iResult == 1)
   {
-    Serial.println("Check wiring")
+    Serial.println("Check wiring");
   }
-
-  setupESC();
+  else
+  {
+    setupESC();
+    delay(3000);
+    Serial.println("Done");
+  }
 }
 
 void setupESC(void)
 {
-
-  motA.attach(4, MIN_PULSE_LENGTH, MAX_PULSE_LENGTH);
-  motB.attach(5, MIN_PULSE_LENGTH, MAX_PULSE_LENGTH);
-  motC.attach(6, MIN_PULSE_LENGTH, MAX_PULSE_LENGTH);
-  motD.attach(7, MIN_PULSE_LENGTH, MAX_PULSE_LENGTH);
-
+  motors.motFL.attach(4, MIN_PULSE_LENGTH, MAX_PULSE_LENGTH);
+  motors.motFR.attach(5, MIN_PULSE_LENGTH, MAX_PULSE_LENGTH);
+  motors.motBL.attach(6, MIN_PULSE_LENGTH, MAX_PULSE_LENGTH);
+  motors.motBR.attach(7, MIN_PULSE_LENGTH, MAX_PULSE_LENGTH);
   displayInstructions();
 }
 
@@ -42,16 +54,22 @@ void loop(void)
     // 0
     case 48:
       Serial.println("Sending minimum throttle");
-      motA.writeMicroseconds(MIN_PULSE_LENGTH);
-      motB.writeMicroseconds(MIN_PULSE_LENGTH);
-      motC.writeMicroseconds(MIN_PULSE_LENGTH);
-      motD.writeMicroseconds(MIN_PULSE_LENGTH);
+      motors.motFL.writeMicroseconds(MIN_PULSE_LENGTH);
+      motors.motBR.writeMicroseconds(MIN_PULSE_LENGTH);
+      motors.motFR.writeMicroseconds(MIN_PULSE_LENGTH);
+      motors.motBL.writeMicroseconds(MIN_PULSE_LENGTH);
       break;
 
     // 1
     case 49:
+      Serial.print("Running less throttle in 3");
+      delay(1000);
+      Serial.print(" 2");
+      delay(1000);
+      Serial.println(" 1...");
+      delay(1000);
       Serial.println("Sending maximum throttle");
-      applyMaxThrottle();
+      sendMaxThrottle();
       break;
 
     // 2
@@ -64,7 +82,41 @@ void loop(void)
       delay(1000);
       test();
       break;
+
+    // 3
+    case 51:
+      Serial.print("Running slow throttle in 3");
+      delay(1000);
+      Serial.print(" 2");
+      delay(1000);
+      Serial.println(" 1...");
+      delay(1000);
+      motors.motFL.writeMicroseconds(1325);
+      motors.motBR.writeMicroseconds(1325);
+      motors.motFR.writeMicroseconds(1325);
+      motors.motBL.writeMicroseconds(1325);
+      delay(3000);
+
+      break;
+
+    // 4
+    case 52:
+      Serial.print("going front in 3");
+      delay(1000);
+      Serial.print(" 2");
+      delay(1000);
+      Serial.println(" 1...");
+      delay(1000);
+      sendMaxThrottle();
+      delay(3000);
+      void goFront();
+      delay(3000);
+      break;
     }
+  }
+  else
+  {
+    Serial.println("Serial Not Available");
   }
 }
 
@@ -78,19 +130,16 @@ void test(void)
     Serial.print("Pulse length Incrementing= ");
     Serial.println(i);
 
-    motA.writeMicroseconds(i);
-    motB.writeMicroseconds(i);
-    motC.writeMicroseconds(i);
-    motD.writeMicroseconds(i);
+    motors.motFL.writeMicroseconds(i);
+    motors.motBR.writeMicroseconds(i);
+    motors.motFR.writeMicroseconds(i);
+    motors.motBL.writeMicroseconds(i);
 
     delay(200);
   }
 
   Serial.println("STOPPING");
-  motA.writeMicroseconds(MIN_PULSE_LENGTH);
-  motB.writeMicroseconds(MIN_PULSE_LENGTH);
-  motC.writeMicroseconds(MIN_PULSE_LENGTH);
-  motD.writeMicroseconds(MIN_PULSE_LENGTH);
+  autoStop();
 }
 
 /**
@@ -99,9 +148,11 @@ void test(void)
 void displayInstructions(void)
 {
   Serial.println("READY - PLEASE SEND INSTRUCTIONS AS FOLLOWING :");
-  Serial.println("\t0 : Send min throttle");
-  Serial.println("\t1 : Send max throttle");
-  Serial.println("\t2 : Run test function\n");
+  Serial.println("\t0 : Send Min Throttle");
+  Serial.println("\t1 : Send Max Throttle");
+  Serial.println("\t2 : Run Test Function");
+  Serial.println("\t3 : Run Slow Throttle");
+  Serial.println("\t4 : Run Test Function\n");
 }
 
 /**
@@ -109,16 +160,16 @@ void displayInstructions(void)
  */
 int checkWiring()
 {
-  if (motA.attached() == 4)
+  if (motors.motFL.attached() == 4)
   {
     Serial.println("FL attached to D4");
-    if (motB.attached() == 5)
+    if (motors.motFR.attached() == 5)
     {
       Serial.println("FR attached to D5");
-      if (motC.attached() == 6)
+      if (motors.motBL.attached() == 6)
       {
         Serial.println("BL attached to D6");
-        if (motD.attached() == 7)
+        if (motors.motBR.attached() == 7)
         {
           Serial.println("BR attached to D7");
           return 0;
@@ -154,19 +205,22 @@ int checkWiring()
 
 void autoStop()
 {
-  applyMaxThrottle();
-  delay(500);
-  for (int i = MAX_PULSE_LENGTH; i >= MIN_PULSE_LENGTH; i -= 5)
+  motors.motFL.writeMicroseconds(1600);
+  motors.motFR.writeMicroseconds(1600);
+  motors.motBL.writeMicroseconds(1600);
+  motors.motBR.writeMicroseconds(1600);
+
+  for (int i = 1600; i >= MIN_PULSE_LENGTH; i -= 5)
   {
     Serial.print("Pulse length Decrementing = ");
     Serial.println(i);
 
-    motA.writeMicroseconds(i);
-    motB.writeMicroseconds(i);
-    motC.writeMicroseconds(i);
-    motD.writeMicroseconds(i);
+    motors.motFL.writeMicroseconds(i);
+    motors.motFR.writeMicroseconds(i);
+    motors.motBL.writeMicroseconds(i);
+    motors.motBR.writeMicroseconds(i);
 
-    delay(200);
+    delay(500);
   }
   Serial.println("STOPPED");
 }
@@ -175,62 +229,68 @@ void autoStop()
  * To Go Front, Back, Left, Right respectivily
  */
 
+/**
+ * Go Front
+ */
+
 void goFront()
 {
   Serial.println("Going Front");
   delay(500);
-  applyMaxThrottle();
+  sendMaxThrottle();
+  delay(2000);
+  motors.motFL.writeMicroseconds(1700);
+  motors.motFR.writeMicroseconds(1700);
   delay(3000);
-  motA.writeMicroseconds(1700);
-  motB.writeMicroseconds(1700);
-  delay(3000);
-  autoStop();
 }
-
+/**
+ * Go Back
+ */
 void goBack()
 {
   Serial.println("Going Back");
   delay(500);
-  applyMaxThrottle();
+  sendMaxThrottle();
+  delay(2000);
+  motors.motBL.writeMicroseconds(1700);
+  motors.motBR.writeMicroseconds(1700);
   delay(3000);
-  motC.writeMicroseconds(1700);
-  motD.writeMicroseconds(1700);
-  delay(3000);
-  autoStop();
 }
-
+/**
+ * Go Left
+ */
 void goLeft()
 {
   Serial.println("Going Left");
   delay(500);
-  applyMaxThrottle();
+  sendMaxThrottle();
+  delay(2000);
+  motors.motFL.writeMicroseconds(1700);
+  motors.motBL.writeMicroseconds(1700);
   delay(3000);
-  motA.writeMicroseconds(1700);
-  motC.writeMicroseconds(1700);
-  delay(3000);
-  autoStop();
 }
-
+/**
+ * Go Right
+ */
 void goRight()
 {
   Serial.println("Going Right");
-  delay(500);
-  applyMaxThrottle();
+  delay(1000);
+  sendMaxThrottle();
   delay(2000);
-  motB.writeMicroseconds(1700);
-  motD.writeMicroseconds(1700);
+  motors.motBR.writeMicroseconds(1700);
+  motors.motFR.writeMicroseconds(1700);
   delay(3000);
-  autoStop();
 }
 
 /**
  * Give max throttle
  */
 
-void applyMaxThrottle()
+void sendMaxThrottle()
 {
-  motA.writeMicroseconds(MAX_PULSE_LENGTH);
-  motB.writeMicroseconds(MAX_PULSE_LENGTH);
-  motC.writeMicroseconds(MAX_PULSE_LENGTH);
-  motD.writeMicroseconds(MAX_PULSE_LENGTH);
+  motors.motFL.writeMicroseconds(MAX_PULSE_LENGTH);
+  motors.motFR.writeMicroseconds(MAX_PULSE_LENGTH);
+  motors.motBL.writeMicroseconds(MAX_PULSE_LENGTH);
+  motors.motBR.writeMicroseconds(MAX_PULSE_LENGTH);
 }
